d7738b1cd9e229e371a90a59a62166e8b4e83e89
grinya37 <112849355+grinya37@users.noreply.github.com>
2023-08-26 23:09:03 +0300

    add_project

---
+++ IMBD_Clone_new/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IMBD_Clone_new
+//
+//  Created by Николай Гринько on 26.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
